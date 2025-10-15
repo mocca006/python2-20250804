ece7a825287d81ba8d0fb840f858db17184a02be
mocca006 <monicachou.688@gmail.com>
2025-10-15 14:20:23 +0800

    20251015

--- python2-pro/demo.py
+++ python2-pro/demo.py
@@ -4,3 +4,5 @@ import streamlit as st
 
 st.title("demo Page --- Hello")
 st.write("This is the demo page of the python2-pro project.")
+st.write("This is a simple demo to show how to use Streamlit.")
+st.write("You can add more content here as you like.")
